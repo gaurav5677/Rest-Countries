dc22806b0b6a60c11a0acf1f0421c06e48d803be
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-05-01 19:03:58 +0530

    Ajax Call

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+/////////////////////////////////////// Ajax call  /////////////////////////////////////// 
 const getCountryData = function (country) {
    const request = new XMLHttpRequest();
    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
